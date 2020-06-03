0ea62f9797d93a045efed4ded04b8b2b00fe9e80
QianyueMa <amandama215@gmail.com>
2020-06-02 23:53:35 -0400

    python-challenge

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,7 +17,6 @@ print(f"Total Votes: {num_votes}")
 
 
 # A complete list of candidates who received votes as well as their winning percentage and the total number of votes each candidate won
-#   use list.append() to track the list
 with open(csvpath) as csvfile:
     reader = csv.reader(csvfile)
     csv_header = next(reader)
@@ -26,13 +25,12 @@ with open(csvpath) as csvfile:
     potential_candidates = ["Khan", "Correy", "Li", "O'Tooley"]
     Khan_votes = 0
     Correy_votes = 0
-    gained_votes = {}
-'''
-    winning_candidate = ""
-    winning_count = 0
-'''
+    Li_votes = 0
+    OTooley_votes = 0
 
-    for i in potential_candidates:
+
+    for i in open(csvpath):
+        #the_candidate = row[2]
         if i == "Khan":
             Khan_votes = Khan_votes + 1
         elif i == "Correy":
@@ -42,44 +40,33 @@ with open(csvpath) as csvfile:
         elif i == "O'Tooley":
             OTooley_votes = OTooley_votes + 1
 
-
-
-    for row in open(csvpath):
-        num_votes = num_votes + 1
-        the_candidate = row[2]
-        if the_candidate not in potential_candidates:
-            potential_candidates.append(the_candidate)
-
-            gained_votes[the_candidate] = 0
-
-        gained_votes[the_candidate] = gained_votes[the_candidate] + 1
-
-    for i in gained_votes:
-        votes = gained_votes.get(i)
-        vote_percentage = float(votes) / float(num_votes) * 100
-        if (votes > winning_count):
-            winning_count = votes 
-            winning_candidate = i
-print(winning_candidate)
-    
-
-    
-
-
-
-
-
+percent_Khan = Khan_votes / num_votes * 100
+percent_Correy = Correy_votes / num_votes * 100
+percent_Li = Li_votes / num_votes * 100
+percent_OTooley = OTooley_votes / num_votes * 100
 
 
 # The winner of the election based on popular vote.
+percentage_list = [percent_Khan, percent_Correy, percent_Li, percent_OTooley]
+winner = max(percentage_list)
 
 
-'''
 # Export the outputs; the txt file is in the "analysis" folder
-output = (
-    f"\n\nElection Results\n" 
+export_output = (
+    f"\nElection Results\n" 
     f"-------------------------\n" 
-    f"Total Votes: {total_votes}\n" 
-    f"-------------------------\n")
-print(output, end="")
-'''
+    f"Total Votes: {num_votes}\n" 
+    f"-------------------------\n"
+    f"Khan: {percent_Khan}% ({Khan_votes})\n"
+    f"Correy: {percent_Correy}% ({Correy_votes})\n"
+    f"Li: {percent_Li}% ({Li_votes})\n"
+    f"O'Tooley: {percent_OTooley}% ({OTooley_votes})\n"
+    f"-------------------------\n"
+    f"Winner: {winner}\n"
+    f"-------------------------\n"
+    )
+
+print (export_output)
+
+with open(outputpath, "w") as txt_file:
+    txt_file.write(export_output)
